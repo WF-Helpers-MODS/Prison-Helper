f0bb6953df04acf68aaa409ef257403b932dbfe0
Alexandr-Botovod <aabramenko6@gmail.com>
2024-11-09 20:49:07 +0600

    обновление

--- Prison Helper.lua
+++ Prison Helper.lua
@@ -3,7 +3,7 @@
 script_name("Prison Helper")
 script_description('Скрипт для Тюрьмы Строгого Режима LV')
 script_author("WF Helpers MODS")
-script_version("0.0.1")
+script_version("0.0.2")
 
 require('lib.moonloader')
 require ('encoding').default = 'CP1251'
